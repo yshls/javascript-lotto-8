2a6c61f5b9eb511369dc02d51047566ecea7a53c
yshls <shluxnsal01@gmail.com>
2025-11-01 20:26:17 +0900

    feat(input): Implement purchase amount input and validation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
+import { UserInput } from './UserInput.js';
+import { UserOutput } from './UserOutput.js';
+import { validatePurchaseAmount } from './Validator.js';
+
 class App {
-  async run() {}
+  #purchaseAmount;
+
+  async run() {
+    await this.#getPurchaseAmount();
+  }
+
+  async #getPurchaseAmount() {
+    while (true) {
+      try {
+        UserOutput.promptPurchaseAmount();
+        const amountInput = await UserInput.readPurchaseAmount();
+
+        validatePurchaseAmount(amountInput);
+
+        this.#purchaseAmount = Number(amountInput);
+        break;
+      } catch (error) {
+        UserOutput.printError(error.message);
+      }
+    }
+  }
 }
 
 export default App;
