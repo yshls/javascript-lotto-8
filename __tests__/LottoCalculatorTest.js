a3194ab1d9af51e229cc02d568dd819203a7a66a
yshls <shluxnsal01@gmail.com>
2025-11-01 22:13:32 +0900

    test(calculator): Clarify test names to use 'match count' instead of 'rank'

--- __tests__/LottoCalculatorTest.js
+++ __tests__/LottoCalculatorTest.js
@@ -7,37 +7,37 @@ describe('로또 계산기 테스트', () => {
 
   const calculator = new LottoCalculator(winningNumbers, bonusNumber);
 
-  test('5등 (3개 일치)을 정확히 계산한다.', () => {
+  test('3개 일치을 정확히 계산한다.', () => {
     const myLotto = new Lotto([1, 2, 3, 10, 11, 12]);
     const rank = calculator.calculateRank(myLotto);
     expect(rank).toBe(5);
   });
 
-  test('4등 (4개 일치)을 정확히 계산한다.', () => {
+  test('4개 일치을 정확히 계산한다.', () => {
     const myLotto = new Lotto([1, 2, 3, 4, 11, 12]);
     const rank = calculator.calculateRank(myLotto);
     expect(rank).toBe(4);
   });
 
-  test('3등 (5개 일치, 보너스 불일치)을 정확히 계산한다.', () => {
+  test('5개 일치, 보너스 불일치를 정확히 계산한다.', () => {
     const myLotto = new Lotto([1, 2, 3, 4, 5, 8]);
     const rank = calculator.calculateRank(myLotto);
     expect(rank).toBe(3);
   });
 
-  test('2등 (5개 일치 + 보너스 일치)을 정확히 계산한다.', () => {
+  test('5개 일치 + 보너스 일치를 정확히 계산한다.', () => {
     const myLotto = new Lotto([1, 2, 3, 4, 5, 7]);
     const rank = calculator.calculateRank(myLotto);
     expect(rank).toBe(2);
   });
 
-  test('1등 (6개 일치)을 정확히 계산한다.', () => {
+  test('6개 일치를 정확히 계산한다.', () => {
     const myLotto = new Lotto([1, 2, 3, 4, 5, 6]);
     const rank = calculator.calculateRank(myLotto);
     expect(rank).toBe(1);
   });
 
-  test('꽝 (2개 이하 일치)을 정확히 계산한다.', () => {
+  test('2개 이하 일치를 정확히 계산한다.', () => {
     const myLotto = new Lotto([1, 2, 10, 11, 12, 13]);
     const rank = calculator.calculateRank(myLotto);
     expect(rank).toBe(null);
