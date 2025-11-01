ac902f1ab715b2c68ee1ffe6c877d46c5d619514
yshls <shluxnsal01@gmail.com>
2025-11-01 21:55:55 +0900

    test(lotto): Add unit test for getSortedNumbers

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,18 +1,23 @@
-import Lotto from "../src/Lotto";
+import Lotto from '../src/Lotto';
 
-describe("로또 클래스 테스트", () => {
-  test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
+describe('로또 클래스 테스트', () => {
+  test('로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.', () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 6, 7]);
-    }).toThrow("[ERROR]");
+    }).toThrow('[ERROR]');
   });
 
-  // TODO: 테스트가 통과하도록 프로덕션 코드 구현
-  test("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.", () => {
+  test('로또 번호에 중복된 숫자가 있으면 예외가 발생한다.', () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 5]);
-    }).toThrow("[ERROR]");
+    }).toThrow('[ERROR]');
   });
 
-  // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+  test('getSortedNumbers가 번호를 오름차순으로 정렬하여 반환한다.', () => {
+    const lotto = new Lotto([6, 5, 4, 3, 2, 1]);
+
+    const sortedNumbers = lotto.getSortedNumbers();
+
+    expect(sortedNumbers).toEqual([1, 2, 3, 4, 5, 6]);
+  });
 });
